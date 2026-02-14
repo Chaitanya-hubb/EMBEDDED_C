1d91156228181b668246be9364bc147404072ab5
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-02-14 18:27:02 +0530

    Added LED Blinking code

---
+++ 01_LED_BLINK.c
@@ -0,0 +1,23 @@
+#include <xc.h>
+
+#pragma config WDTE = 0FF      // Watchdog Timer disabled
+#define __XTAL_FREQ  4000000   // 4 MHz crstal frequency
+
+static void init_congit(void)
+{
+   TRISB = 0x00;  //set all pins as output 
+}
+
+void main()
+{
+   init_config();
+   
+   while(1)
+   {
+       PORTB = 0xFF;     //TURN ON all LEds on PORTB
+       __delay_ms(500);
+      PORTB = 0x00;     // TURN OFF all LEDs on PORTB
+      __delay_ms(500);
+   }
+   return;
+}
